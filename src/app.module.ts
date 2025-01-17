ecfd5c634b8004c709e7d820080285d2a8fbdfaf
Herman2201 <ushkalo.herman@gmail.com>
2025-01-17 20:20:05 +0200

    change throttling time

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,7 @@ import { CourseModule } from "./course/course.module";
     ThrottlerModule.forRoot([
       {
         ttl: 60000,
-        limit: 10,
+        limit: 1000,
       },
     ]),
   ],
